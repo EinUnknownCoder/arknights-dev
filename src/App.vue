473f197141478409083b53e256d1aac3c1dafc88
EinUnknownCoder <einunknowncoder@gmail.com>
2020-11-27 11:07:36 +0100

    V1.0

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="black"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Arknights Logo"
+          class="shrink mr-2"
+          contain
+          src="https://webusstatic.yo-star.com/ark_us_web/pc/img/logo02.924e2f2a.png"
+          transition="scale-transition"
+          height="50"
+          position="left"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://docs.google.com/spreadsheets/d/e/2PACX-1vS5yhjGa6F9UtgTceIUyyqUVSzo7-JwRk3W7BfgJ4ZEikuFDVG1y_MfUic3hYn5Gdh2wymjMm_SoXfj/pubhtml#"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Lookup</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <HelloWorld/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld';
+
+export default {
+  name: 'App',
+
+  components: {
+    HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="12" >
+        <h2>Sanity Calculator</h2>
+      </v-col>
+
+      <v-col cols="12" md="4">
+        <v-text-field
+          label="Maximum Sanity"
+          v-model="maxSanity"
+          clearable
+          prepend-icon="mdi-head-flash"
+        >
+        </v-text-field>
+      </v-col>
+
+      <v-col cols="12" md="4">
+        <v-text-field
+          label="Reset Hour"
+          v-model="resetHour"
+          clearable
+          prepend-icon="mdi-hours-24"
+        ></v-text-field>
+      </v-col>
+
+      <v-col cols="12" md="4">
+        <v-alert type="info">Your Sanity should be below {{ allowedSanity }}</v-alert>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+
+  data: () => ({
+    maxSanity: 128,
+    resetHour: 12,
+  }),
+
+  computed: {
+    allowedSanity() {
+      return this.maxSanity - Math.floor(((this.resetHour * 60) - ((new Date().getHours() * 60) + (new Date().getMinutes()))) / 5) + 1
+    }
+  }
+};
+</script>
